0f31ddbb2311e9b324469d63f1e098fcf6529407
Ryan <ryan.simao72@gmail.com>
2026-04-17 22:48:54 -0300

    FEITO

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Digite sua altura(m) e seu peso(Kg),cada um em uma linha: ");
+string alturadigitada = Console.ReadLine()!;
+string pesodigitado = Console.ReadLine()!;
+
+double altura = Convert.ToDouble (alturadigitada);
+double peso = Convert.ToDouble (pesodigitado);
+
+double imc = peso / Math.Pow (altura, 2) ;
+
+Console.WriteLine ("Seu IMC é: {imc}");
+
+
+
+
